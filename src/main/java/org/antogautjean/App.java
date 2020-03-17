5984a0248939db6568e238e6a5bf0aa86f66bd8e
GauthierNelkin <GauthierNelkin@DESKTOP-432QBD4>
2020-03-17 19:07:32 +0100

    Fixed error FileImporter in main

--- src/main/java/org/antogautjean/App.java
+++ src/main/java/org/antogautjean/App.java
@@ -11,7 +11,7 @@ public class App {
 
 		Stock stock = new Stock("Principal");
 
-		new FileImporter("./src/main/java/org/antogautjean/files/elements.csv", "./src/main/java/org/antogautjean/files/prix.csv", stock);
+		new FileImporter("./src/main/java/org/antogautjean/data/elements.csv","./src/main/java/org/antogautjean/data/prix.csv", stock);
 
 		System.out.println(stock.getProduct("E002").toString());
 
