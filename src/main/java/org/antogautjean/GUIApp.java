8cc9bee015e885d3560fd82bdfbe768e289660dd
Antony Laget <antony.laget@icloud.com>
2020-03-17 19:51:28 +0100

    -- on the road to mvc  --L.O.L--

--- src/main/java/org/antogautjean/GUIApp.java
+++ src/main/java/org/antogautjean/GUIApp.java
@@ -1,22 +1,15 @@
 package org.antogautjean;
 
-import javax.swing.*;
+import org.antogautjean.model.Stock;
+
+import java.io.IOException;
 
 public class GUIApp {
-    private JButton TESTButton;
-    private JPanel Home;
-    private JTabbedPane Dashboard;
-    private JLabel globalState;
 
-    public GUIApp() {
+    public static void main(String[] args) throws IOException {
 
-    }
+        GUI gui = new GUI();
+        gui.startGUI();
 
-    public static void main(String[] args){
-        JFrame frame = new JFrame("Factory");
-        frame.setContentPane(new GUIApp().Home);
-        frame.setSize(1280, 720);
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setVisible(true);
     }
 }
