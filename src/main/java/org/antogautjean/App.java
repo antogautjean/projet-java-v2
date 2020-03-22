2848415d8e584763dd54d046611cff67c499ebac
Jean Z. SORDES <john.sordes@gmail.com>
2020-03-22 19:23:50 +0100

    update csv

--- src/main/java/org/antogautjean/App.java
+++ src/main/java/org/antogautjean/App.java
@@ -8,29 +8,29 @@ import java.io.IOException;
 
 public class App {
 
-	public static void main(String[] args) throws IOException {
-		// init depending of args
-		boolean isUIVisible = true;
+    public static void main(String[] args) throws IOException {
+        // init depending of args
+        boolean isUIVisible = true;
 
-		for (String s : args) {
-			if (s.equals("--no-ui")) {
-				isUIVisible = false;
-			}
-		}
+        for (String s : args) {
+            if (s.equals("--no-ui")) {
+                isUIVisible = false;
+            }
+        }
 
-		System.out.println("starting : OK");
+        System.out.println("starting : OK");
 
-		if (isUIVisible) {
-			new HomeView();
-			System.out.println("Runing with UI");
-		} else {
-			System.out.println("Runing with NO UI");
-		}
+        if (isUIVisible) {
+            new HomeView();
+            System.out.println("Runing with UI");
+        } else {
+            System.out.println("Runing with NO UI");
+        }
 
-		StockController koalaController = new StockController("koala");
+        StockController koalaController = new StockController("koala");
 
-		System.out.println("FileImporter : Reading CSV Stock file");
-		FileImporter.fileToStock("./src/main/java/org/antogautjean/data/elements.csv",
-				"./src/main/java/org/antogautjean/data/prix.csv", koalaController);
-	}
+        System.out.println("FileImporter : Reading CSV Stock file");
+        FileImporter.fileToStock("./src/main/java/org/antogautjean/data/elements.csv",
+                "./src/main/java/org/antogautjean/data/prix.csv", koalaController);
+    }
 }
