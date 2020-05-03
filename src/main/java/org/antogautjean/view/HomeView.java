6f5bd310414a518d4f9d9ef16bfb95599daa2af0
Antony <antony.laget@icloud.com>
2020-05-03 23:39:33 +0200

    -- begin homeview refactor --

--- src/main/java/org/antogautjean/view/HomeView.java
+++ src/main/java/org/antogautjean/view/HomeView.java
@@ -7,7 +7,6 @@ import java.util.Vector;
 import javax.swing.BorderFactory;
 import javax.swing.ImageIcon;
 import javax.swing.JFrame;
-import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JSplitPane;
@@ -37,73 +36,89 @@ public class HomeView {
     protected DefaultTableModel stockTableModel;
     protected DefaultTableModel linesTableModel;
 
-    public HomeView(StockController stockList, FactoryController lineList) {
+    private void configJPanel(JPanel panel){
+        panel.setBorder(new EmptyBorder(10, 10, 10, 10)); // rajoute des marges
+        panel.setLayout(new BorderLayout());
+        panel.setMinimumSize(new Dimension(300,200));
+    }
 
-        JFrame frame = new JFrame("Factory");
-        frame.setExtendedState(JFrame.MAXIMIZED_BOTH);
-        frame.setMinimumSize(new Dimension(1200,500));
-        frame.setTitle("Gestionnaire de production");
-        frame.setLocationRelativeTo(null);
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setLayout(new BorderLayout());
-        frame.setIconImage(new ImageIcon("./src/main/java/org/antogautjean/data/factory_icon.png").getImage());
 
-        JPanel topPanel = new JPanel();
-        topPanel.setBorder(new EmptyBorder(10, 10, 10, 10)); // rajoute des marges
-        topPanel.setLayout(new BorderLayout());
-        topPanel.setMinimumSize(new Dimension(300,200));
+    private void configStockTable(JPanel topPanel, String[] stockColumns) {
 
-        JPanel bottomPanel = new JPanel();
-        bottomPanel.setBorder(new EmptyBorder(10, 10, 10, 10)); // rajoute des marges
-        bottomPanel.setLayout(new BorderLayout());
-        bottomPanel.setMinimumSize(new Dimension(300,200));
+        configJPanel(topPanel);
 
-        this.stockList = stockList;
-        this.linesList = lineList;
-
-        // Stock panel - Top
-        final String[] stockColumns = new String[] { "Code", "Nom", "Quantité actuelle", "Quantité à acheter",
-                "Coût d'achat prévisionnel", "Nouvelle quantité après achat", "Quantité simulée après calcul" };
         this.stockTableModel = new StockTableModel(new Vector<>(), new Vector<>(Arrays.asList(stockColumns)));
         this.stockTable = new CustomJTable(stockTableModel);
         this.stockTable.setRowHeight(30);
 
-        DefaultTableCellRenderer cellRenderer = new DefaultTableCellRenderer() {
-            public Component getTableCellRendererComponent(JTable table, Object value, boolean isSelected,
-                    boolean hasFocus, int row, int column) {
+        TableColumnModel columnModel = this.stockTable.getColumnModel();
+        this.stockTable.setDefaultRenderer(Object.class, createCellRenderer());
+        columnModel.getColumn(0).setPreferredWidth(50);
+        columnModel.getColumn(1).setPreferredWidth(300);
+        columnModel.getColumn(3).setPreferredWidth(100);
+
+        topPanel.add(new JScrollPane(this.stockTable));
+    }
+
+    private DefaultTableCellRenderer createCellRenderer(){
+        return new DefaultTableCellRenderer() {
+            public Component getTableCellRendererComponent(JTable table, Object value, boolean isSelected, boolean hasFocus, int row, int column) {
                 super.getTableCellRendererComponent(table, value, isSelected, hasFocus, row, column);
                 // setHorizontalAlignment(JLabel.CENTER);
                 setBorder(BorderFactory.createEmptyBorder(0, 5, 0, 5));
                 return this;
             }
         };
-        TableColumnModel columnModel = this.stockTable.getColumnModel();
-        this.stockTable.setDefaultRenderer(Object.class, cellRenderer);
-        columnModel.getColumn(0).setPreferredWidth(50);
-        columnModel.getColumn(1).setPreferredWidth(300);
-        columnModel.getColumn(3).setPreferredWidth(100);
+    }
 
-        topPanel.add(new JScrollPane(this.stockTable));
+    private JFrame mainFrame;
 
-        // Lines panel - Bottom
-        final String[] linesColumns = new String[] { "Ordre de vérification", "Code", "Nom", "Code éléments en sortie",
-                "Niveau d'activation", "Etat de la chaîne", "Quantité produite / quantité demandée" };
+    public void frameInit(){
+        this.mainFrame = new JFrame("Factory");
+        this.mainFrame.setExtendedState(JFrame.MAXIMIZED_BOTH);
+        this.mainFrame.setMinimumSize(new Dimension(1200,500));
+        this.mainFrame.setTitle("Gestionnaire de production");
+        this.mainFrame.setLocationRelativeTo(null);
+        this.mainFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.mainFrame.setLayout(new BorderLayout());
+        this.mainFrame.setIconImage(new ImageIcon("./src/main/java/org/antogautjean/data/factory_icon.png").getImage());
+    }
+
+
+    public HomeView(StockController stockList, FactoryController lineList) {
+
+        frameInit();
+
+        this.stockList = stockList;
+        this.linesList = lineList;
+        final String[] stockColumns = new String[] { "Code", "Nom", "Quantité actuelle", "Quantité à acheter", "Coût d'achat prévisionnel", "Nouvelle quantité après achat", "Quantité simulée après calcul" };
+        final String[] linesColumns = new String[] { "Ordre de vérification", "Code", "Nom", "Code éléments en sortie", "Niveau d'activation", "Etat de la chaîne", "Quantité produite / quantité demandée" };
+
+        JPanel topPanel = new JPanel();
+        JPanel bottomPanel = new JPanel();
+
+        configJPanel(bottomPanel);
+
+        configStockTable(topPanel, stockColumns);
+
+
         this.linesTableModel = new LinesTableModel(new Vector<>(), new Vector<>(Arrays.asList(linesColumns)));
         this.linesTable = new CustomJTable(linesTableModel);
         this.linesTable.setRowHeight(30);
-        this.linesTable.setDefaultRenderer(Object.class, cellRenderer); // adds padding
+        this.linesTable.setDefaultRenderer(Object.class, createCellRenderer()); // adds padding
         bottomPanel.add(new JScrollPane(this.linesTable));
 
         configPanel(this.stockTable, this.stockTableModel, this.stockList);
         configPanel(this.linesTable, this.linesTableModel, this.linesList);
 
         JSplitPane splitPane = new JSplitPane(JSplitPane.VERTICAL_SPLIT, topPanel, bottomPanel);
-        
-        frame.getContentPane().add(splitPane);
-        frame.setVisible(true);
+
+        this.mainFrame.getContentPane().add(splitPane);
+        this.mainFrame.setVisible(true);
 
         splitPane.setDividerLocation(.5);
     }
+
 
     private void configPanel(CustomJTable cjt, DefaultTableModel ctm, TableLinesFormatInterface factory) {
         cjt.getTableHeader().setReorderingAllowed(true);
