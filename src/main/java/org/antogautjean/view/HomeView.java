5afaee2cac9582f5efbfdeae01475aabb5aa880c
Jean Z. SORDES <john.sordes@gmail.com>
2020-05-03 21:17:28 +0200

    design improved

--- src/main/java/org/antogautjean/view/HomeView.java
+++ src/main/java/org/antogautjean/view/HomeView.java
@@ -1,47 +1,62 @@
 package org.antogautjean.view;
 
-import org.antogautjean.Controller.FactoryController;
-import org.antogautjean.Controller.StockController;
-
 import java.awt.BorderLayout;
 import java.util.Arrays;
 import java.util.Vector;
 
-import javax.swing.*;
-
+import javax.swing.BorderFactory;
+import javax.swing.ImageIcon;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JSplitPane;
+import javax.swing.JTabbedPane;
+import javax.swing.JTable;
+import javax.swing.ListSelectionModel;
 import javax.swing.border.EmptyBorder;
 import javax.swing.table.DefaultTableCellRenderer;
 import javax.swing.table.DefaultTableModel;
 import javax.swing.table.TableColumnModel;
+
+import org.antogautjean.Controller.FactoryController;
+import org.antogautjean.Controller.StockController;
+
+import java.awt.Component;
+import java.awt.Dimension;
 
 public class HomeView {
 
     protected StockController stockList;
     protected FactoryController linesList;
 
+    protected JTabbedPane onglets;
+
     protected CustomJTable stockTable;
     protected CustomJTable linesTable;
-    final DefaultTableModel stockTableModel;
-    final DefaultTableModel linesTableModel;
+    protected DefaultTableModel stockTableModel;
+    protected DefaultTableModel linesTableModel;
 
     public HomeView(StockController stockList, FactoryController lineList) {
 
         JFrame frame = new JFrame("Factory");
         frame.setExtendedState(JFrame.MAXIMIZED_BOTH);
+        frame.setMinimumSize(new Dimension(1200,500));
         frame.setTitle("Gestionnaire de production");
         frame.setLocationRelativeTo(null);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setLayout(new BorderLayout());
+        frame.setIconImage(new ImageIcon("./src/main/java/org/antogautjean/data/factory_icon.png").getImage());
 
         JPanel topPanel = new JPanel();
         topPanel.setBorder(new EmptyBorder(10, 10, 10, 10)); // rajoute des marges
         topPanel.setLayout(new BorderLayout());
-        frame.getContentPane().add(topPanel, BorderLayout.NORTH);
+        topPanel.setMinimumSize(new Dimension(300,200));
 
         JPanel bottomPanel = new JPanel();
-        bottomPanel.setBorder(new EmptyBorder(0, 10, 10, 10)); // rajoute des marges
+        bottomPanel.setBorder(new EmptyBorder(10, 10, 10, 10)); // rajoute des marges
         bottomPanel.setLayout(new BorderLayout());
-        frame.getContentPane().add(bottomPanel, BorderLayout.CENTER);
+        bottomPanel.setMinimumSize(new Dimension(300,200));
 
         this.stockList = stockList;
         this.linesList = lineList;
@@ -53,38 +68,20 @@ public class HomeView {
         this.stockTable = new CustomJTable(stockTableModel);
         this.stockTable.setRowHeight(30);
 
+        DefaultTableCellRenderer cellRenderer = new DefaultTableCellRenderer() {
+            public Component getTableCellRendererComponent(JTable table, Object value, boolean isSelected,
+                    boolean hasFocus, int row, int column) {
+                super.getTableCellRendererComponent(table, value, isSelected, hasFocus, row, column);
+                // setHorizontalAlignment(JLabel.CENTER);
+                setBorder(BorderFactory.createEmptyBorder(0, 5, 0, 5));
+                return this;
+            }
+        };
         TableColumnModel columnModel = this.stockTable.getColumnModel();
-        DefaultTableCellRenderer centerRenderer = new DefaultTableCellRenderer();
-        centerRenderer.setHorizontalAlignment( JLabel.CENTER );
-
-        ((DefaultTableCellRenderer)this.stockTable.getTableHeader().getDefaultRenderer()).setHorizontalAlignment(JLabel.CENTER);
-
-        columnModel.getColumn(0).setMinWidth(50);
-        columnModel.getColumn(0).setMaxWidth(50);
-        columnModel.getColumn(0).setCellRenderer(centerRenderer);
-
-        columnModel.getColumn(1).setMinWidth(300);
-        columnModel.getColumn(1).setCellRenderer(centerRenderer);
-
-        columnModel.getColumn(2).setMinWidth(100);
-        columnModel.getColumn(2).setMaxWidth(100);
-        columnModel.getColumn(2).setCellRenderer(centerRenderer);
-
-        columnModel.getColumn(3).setMinWidth(120);
-        columnModel.getColumn(3).setMaxWidth(120);
-
-        columnModel.getColumn(4).setMinWidth(150);
-        columnModel.getColumn(4).setMaxWidth(150);
-        columnModel.getColumn(4).setCellRenderer(centerRenderer);
-
-        columnModel.getColumn(5).setMinWidth(180);
-        columnModel.getColumn(5).setMaxWidth(180);
-        columnModel.getColumn(5).setCellRenderer(centerRenderer);
-
-        columnModel.getColumn(6).setMinWidth(160);
-        columnModel.getColumn(6).setMaxWidth(160);
-        columnModel.getColumn(6).setCellRenderer(centerRenderer);
-
+        this.stockTable.setDefaultRenderer(Object.class, cellRenderer);
+        columnModel.getColumn(0).setPreferredWidth(50);
+        columnModel.getColumn(1).setPreferredWidth(300);
+        columnModel.getColumn(3).setPreferredWidth(100);
 
         topPanel.add(new JScrollPane(this.stockTable));
 
@@ -94,12 +91,18 @@ public class HomeView {
         this.linesTableModel = new LinesTableModel(new Vector<>(), new Vector<>(Arrays.asList(linesColumns)));
         this.linesTable = new CustomJTable(linesTableModel);
         this.linesTable.setRowHeight(30);
+        this.linesTable.setDefaultRenderer(Object.class, cellRenderer); // adds padding
         bottomPanel.add(new JScrollPane(this.linesTable));
 
         configPanel(this.stockTable, this.stockTableModel, this.stockList);
         configPanel(this.linesTable, this.linesTableModel, this.linesList);
 
+        JSplitPane splitPane = new JSplitPane(JSplitPane.VERTICAL_SPLIT, topPanel, bottomPanel);
+        
+        frame.getContentPane().add(splitPane);
         frame.setVisible(true);
+
+        splitPane.setDividerLocation(.5);
     }
 
     private void configPanel(CustomJTable cjt, DefaultTableModel ctm, TableLinesFormatInterface factory) {
